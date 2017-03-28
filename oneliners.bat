21d2ca804e250a6c4abd3b4f43057ccb47bdcdd9
Caleb Sullivan <calebsullivan@me.com>
2017-03-28 10:45:03 -0400

    Update oneliners.bat

--- oneliners.bat
+++ oneliners.bat
@@ -8,3 +8,7 @@ mstsc /shadow:0 /v:<HOSTNAME> /control
 :: Find applied, problems with group policy
 
 gpresult /r /scope:computer
+
+:: ps1, last logon date for all computers on DC
+get-adcomputer -filter * -properties * | FT name, lastlogondate -autosize
+ 
